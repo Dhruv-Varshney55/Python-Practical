8b08d59e94ba86030e5684e4854e7005fc36bdbe
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2022-05-30 23:58:01 +0530

    Update

--- Miscellaneous/File Handling/Ques_1.py.py
+++
@@ -1,4 +0,0 @@
-# Write a program in python to read entire content of file (“data.txt”)?
-f = open("data.txt","r")
-l = f.read()
-print(l)
--- Miscellaneous/Math Module/Ques_1.py.py
+++
@@ -1,5 +0,0 @@
-#Write a Python program to convert radian to degree?
-pi=22/7
-radian = float(input("Input radians: "))
-degree = radian*(180/pi)
-print(degree)
