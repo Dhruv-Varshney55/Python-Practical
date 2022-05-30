74efe47133427146b235907953c06433d9aeac51
Dhruv-Varshney55 <dhruvvarshney24112002@gmail.com>
2022-05-30 23:51:00 +0530

    Update

--- Miscellaneous/Exception Handling/Ques_5.py.py
+++
@@ -1,8 +0,0 @@
-#WAP in python to raise Airthmetic exception?
-try:  
-    a = 10/0  
-    print (a)
-except ArithmeticError:  
-        print ("This statement is raising an arithmetic exception.")
-else:  
-    print ("Success.")
